97a77cbad8e5504cd7b32dcdfd87801a0b441351
lucas <yisehui@outlook.com>
2024-12-19 08:40:23 +0800

    1

--- stu_admin_system/settings.py
+++ stu_admin_system/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user.apps.UserConfig",
+    "stu.apps.StuConfig",
+    "tech.apps.TechConfig",
+    "admin.apps.AdminConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,8 @@ ROOT_URLCONF = "stu_admin_system.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates']
+        ,
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
